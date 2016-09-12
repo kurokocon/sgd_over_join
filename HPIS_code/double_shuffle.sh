c7aa0bf7fcdfac03df91c3732e10ae15391ab2dc
kurokocon <fzhan@wisc.edu>
2016-09-12 01:30:49 -0500

    clean up modified code of postgresql

--- HPIS_code/double_shuffle.sh
+++
@@ -1,7 +0,0 @@
-i=0
-while [ $((2**i)) -lt 80000 ]; do
-	echo $((40000 / (2**i)))
-	IFS=$' ' read -r -a arr < $i/param.Rlog
-	./SGDTrain_old 40000 S -n 1 -i ./ R -f 2 -r 4000 -o $i/ -e 100 -a ${arr[1]} -d ${arr[2]} -m 3 -p $((2**i)) -v > $i/test.Tlog 2>&1 &
-	i=$((i+1))
-done
